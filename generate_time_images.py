d925dfa82ef2593b67a959b4b4096105b8fe23c3
Max <mbutunov@gmail.com>
2019-07-17 00:26:42 +0300

    fix error, change font size

--- generate_time_images.py
+++ generate_time_images.py
@@ -2,7 +2,7 @@ import time
 from PIL import ImageDraw, Image, ImageFont
 from datetime import datetime, timedelta
 
-FONT_SIZE = 50
+FONT_SIZE = 70
 TEXT_Y_POSITION = 1
 TEXT_X_POSITION = 1
 MOSCOW_UTC = 3 #for servers heroku
@@ -16,7 +16,7 @@ def change_img():
     text = convert_time_to_string(start_time)
     row = Image.new('RGBA', (200, 200), "black")
     parsed = ImageDraw.Draw(row)
-    font = ImageFont.truetype("DS-DIGIT.ttf", FONT_SIZE)
+    font = ImageFont.truetype("DS-DIGIT.TTF", FONT_SIZE)
     parsed.text((int(row.size[0]*0.2), int(row.size[1]*0.35)), f'{text}', 
                  align="center", font=font, fill=(14,234,18))
     row.save(f'time.png', "PNG")
